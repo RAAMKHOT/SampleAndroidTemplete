b29a385f4734d40320e45aa2a15bcc7bb3a72c71
Ramagouda Khot <ramagouda.khot@infinite.com>
2023-04-20 17:59:30 +0530

    #Login screen changes..

--- app/src/main/java/com/sample/templet/main/view/activities/LoginActivity.kt
+++ app/src/main/java/com/sample/templet/main/view/activities/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.sample.templet.main.view.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
 import androidx.activity.viewModels
+import com.sample.templet.R
 import com.sample.templet.main.view.base.BaseActivity
 import com.sample.templet.main.viewmodel.LoginViewModel
 import com.sample.templet.databinding.ActivityLoginBinding
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener  {
     private val TAG = LoginActivity::class.java.simpleName
     private val viewModel: LoginViewModel by viewModels()
     private lateinit var binding: ActivityLoginBinding
@@ -17,9 +21,39 @@ class LoginActivity : BaseActivity() {
         setContentView(binding.root)
         init()
     }
+    private fun init(){
+        binding.imageViewPasswordVisible.setOnClickListener(this)
+    }
 
-    private fun init() {
-
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.imageViewPasswordVisible -> {
+                onChangePasswordVisibility()
+            }
+        }
+    }
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun onChangePasswordVisibility() {
+        val cursorPosition: Int = binding.editTextPassword.selectionStart
+        if (binding.editTextPassword.inputType == InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+            binding.imageViewPasswordVisible.setImageDrawable(
+                resources.getDrawable(
+                    R.drawable.ic_eye_show,
+                    null
+                )
+            )
+            binding.editTextPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        } else {
+            binding.imageViewPasswordVisible.setImageDrawable(
+                resources.getDrawable(
+                    R.drawable.ic_eye_hide,
+                    null
+                )
+            )
+            binding.editTextPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+        // returns cursor to position
+        binding.editTextPassword.setSelection(cursorPosition)
     }
 
 }
